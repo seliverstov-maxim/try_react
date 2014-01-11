983dbdb85d1b025f34dedf22bb21c4c389b63680
Seliverstov Maxim <maxim.web.developer@gmail.com>
2014-01-11 19:06:26 +0000

    Add initial state. Add buttons and rask list

--- js/todo.js
+++ js/todo.js
@@ -2,6 +2,18 @@
 
 $(function() {
 	var tasklist = React.createClass({
+		getInitialState: function() {
+			return {
+				items: [
+					{id: 0, text: 'Some task', is_enable: false},
+					{id: 1, text: 'Some task', is_enable: true},
+					{id: 2, text: 'Some task', is_enable: false},
+				],
+				count: 0,
+				text: ''
+			};
+		},
+
 		head: function() {
 			return (
 				<div className="head panel panel-primary">
@@ -25,12 +37,82 @@ $(function() {
 			);
 		},
 
-		list: function() {
+		list: function(items) {
 			return (
 				<div className="panel-body">
 					<ul className="list-unstyled">
+						{this.state.items.map(
+							function(item){
+								if (item.is_enable) {
+									return (
+										<li className="panel lead clearfix">
+											{this.enable_item()}
+										</li>
+									)
+								} else {
+									return (
+										<li className="panel lead clearfix">
+											{this.disable_item()}
+										</li>
+									)
+								}
+							}, this)}
 					</ul>
 				</div>
+			);
+		},
+
+		enable_item: function() {
+			return (
+				<div className="form-inline">
+					<div className="col-sm-9">
+						<input type="text" className="form-control" value="Some task"/>
+					</div>
+					<div className="btn-group pull-right">
+						<button type="button" className="btn btn-primary btn-sm">
+							<span className="glyphicon glyphicon-ok"></span>
+						</button>
+						<button type="button" className="btn btn-warning btn-sm">
+							<span className="glyphicon glyphicon-remove"></span>
+						</button>
+					</div>
+				</div>
+			);
+		},
+
+		disable_item: function() {
+			return (
+				<div>
+					<span className="col-sm-8">Some task</span>
+					<div className="btn-group pull-right">
+											{this.button_pencil()}
+											{this.button_remove()}
+					</div>
+				</div>
+				);
+		},
+
+		button_pencil: function() {
+			return (
+				<button type="button" className="btn btn-primary btn-sm">
+					<span className="glyphicon glyphicon-pencil"></span>
+				</button>
+			);
+		},
+
+		button_remove: function() {
+			return (
+				<button type="button" className="btn btn-warning btn-sm">
+					<span className="glyphicon glyphicon-remove"></span>
+				</button>
+			);
+		},
+
+		button_ok: function() {
+			return (
+				<button type="button" class="btn btn-primary btn-sm">
+					<span class="glyphicon glyphicon-ok"></span>
+				</button>
 			);
 		},
 
@@ -45,7 +127,7 @@ $(function() {
 				<div id="task-list">
 					{this.head()}
 					{this.form()}
-					{this.list()}
+					{this.list(this.props.items)}
 				</div>
 			);
 		}
